d3afd70fda9ec46d9dda91d60de009e1d1a024df
eir73 <roflaneir73@gmail.com>
2020-09-03 18:30:37 +0300

    Initial dist subtree commit

---
+++ dist/precache-manifest.d50008f0a48786b6b79324a07c17559f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "688bd57f55d3acce3d3f",
+    "url": "/vue-data-filters/css/app.e9aab7e3.css"
+  },
+  {
+    "revision": "c74ddebb2d9fb56668bb27b76a7a925a",
+    "url": "/vue-data-filters/css/style.css"
+  },
+  {
+    "revision": "a2312d26e2150642d8c705de58e5b67b",
+    "url": "/vue-data-filters/index.html"
+  },
+  {
+    "revision": "688bd57f55d3acce3d3f",
+    "url": "/vue-data-filters/js/app.3f0bf33d.js"
+  },
+  {
+    "revision": "28d63e1606c0b95402e1",
+    "url": "/vue-data-filters/js/chunk-vendors.6b235ad5.js"
+  },
+  {
+    "revision": "eb0716c06a916c8d240f917c43fe8de6",
+    "url": "/vue-data-filters/manifest.json"
+  },
+  {
+    "revision": "1700e549c1847835cabfc62cf2b2f8c1",
+    "url": "/vue-data-filters/mocups/desktop.psd"
+  },
+  {
+    "revision": "ed911920a0d0ec126556c8c4c7d0a89c",
+    "url": "/vue-data-filters/mocups/mobile.psd"
+  },
+  {
+    "revision": "f8935e52e4b004f561766604c8415d5f",
+    "url": "/vue-data-filters/mocups/states.psd"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-data-filters/robots.txt"
+  },
+  {
+    "revision": "cd4ee769afe79892627cb97c04eff69c",
+    "url": "/vue-data-filters/state/data.json"
+  }
+]);
